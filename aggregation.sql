8d1842d000c3b307c8fbb48d5a7f87d92321cb22
Melissa Cantu <meli_cantu12@hotmail.com>
2018-08-20 14:29:15 -0500

    exercise 2 aggregation

---
+++ aggregation.sql
@@ -0,0 +1,15 @@
+-- SELECT * FROM restaurant ORDER BY distance_miles_by_headquarters;
+
+-- SELECT * FROM restaurant ORDER BY distance_miles_by_headquarters LIMIT 2;
+
+-- SELECT * FROM restaurant ORDER BY stars LIMIT 2;
+
+-- SELECT * FROM restaurant ORDER BY stars, distance_miles_by_headquarters <= 2 LIMIT 2;
+
+-- SELECT SUM(stars) FROM restaurant;
+
+-- SELECT COUNT(category_tyoe_of_food) FROM restaurant;
+
+-- SELECT AVG(STARS) FROM restaurant;
+
+-- SELECT MAX(stars) FROM restaurant;
